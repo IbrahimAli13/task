3f6255250d66fe3d0a1213aa902a8ab562058124
ibrahim <ibrahim@DESKTOP-56N6NOL.localdomain>
2022-11-07 21:51:11 +0300

    commit work

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,19 @@
 class Car < ApplicationRecord
     validates :user_id, uniqueness: true , presence: true
-    validates :model, inclusion: { in: 1985..2030, message: 'The Model must be between 1985 and 2030' }, presence: true
+    validates :model, numericality: { in: 1985..2030, message: 'The Model must be between 1985 and 2030' }, presence: true
     validates :car_type, presence: true
     belongs_to :user
+    scope :NewerCars, ->(year) { where("model > ?", year) }
+    scope :OlderCars, ->(year) { where("model < ?", year) }
+
+
+    after_validation :Capitalise, on: :create
+
+    private
+    def Capitalise
+      self.car_type = car_type.upcase
+    end
+   
+
+
 end
