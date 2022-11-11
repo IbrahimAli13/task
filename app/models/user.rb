604941acd3a92dbe84f260f629e24db0a88ba5ce
ibrahim <ibrahim@DESKTOP-56N6NOL.localdomain>
2022-11-11 13:35:22 +0300

    commit work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,21 @@
 class User < ApplicationRecord
-    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/, on: :create }, presence: true, uniqueness: true
-    validates :username, presence: true, uniqueness: true
-    validates :name, presence: true, uniqueness: true
-    has_one :cars
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+       :recoverable, :rememberable, :validatable
+    #validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/, on: :create }, presence: true, uniqueness: true
+    #validates :username, presence: true, uniqueness: true
+    #validates :name, presence: true, uniqueness: true
+    has_one :car
+    scope :UserCarModel, -> (year) {joins(:car).where(car: {"model > ?": year})}
     
 
-    after_validation :Capitalise, on: :create
+    #after_validation :Capitalise, on: :create
 
-    private
-    def Capitalise
-      self.name = name.upcase.titleize
-    end
+   # private
+   # def Capitalise
+    #  self.name = name.upcase.titleize
+   # end
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_03_183616) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_11_090715) do
   create_table "cars", force: :cascade do |t|
     t.integer "user_id"
     t.string "car_type"
@@ -25,6 +25,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_03_183616) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
